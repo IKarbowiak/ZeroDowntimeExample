d70175158bf3585924b3f604450a17bded54b60e
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-13 15:00:42 +0200

    Configure postgres db with docker compose

--- zerodowntime/zerodowntime/settings.py
+++ zerodowntime/zerodowntime/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "zerodowntime.product",
 ]
 
 MIDDLEWARE = [
@@ -69,15 +73,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "zerodowntime.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Maximum time in seconds Django can keep the database connections opened.
+# Set the value to 0 to disable connection persistence, database connections
+# will be closed after each request.
+DB_CONN_MAX_AGE = int(os.environ.get("DB_CONN_MAX_AGE", 600))
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://demo:demo@localhost:5432/demo",
+        conn_max_age=DB_CONN_MAX_AGE,
+    ),
 }
 
 
