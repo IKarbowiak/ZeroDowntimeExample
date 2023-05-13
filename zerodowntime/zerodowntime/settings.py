cf54c7462dcc454970a2671326511ebcf7e14a7e
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-13 21:34:05 +0200

    Update requirements

--- zerodowntime/zerodowntime/settings.py
+++ zerodowntime/zerodowntime/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "zerodowntime.product",
+    "graphene_django",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
