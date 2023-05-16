b209326be1d1bf0d8b21b4ee30b26c14a02c832b
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-16 13:34:15 +0200

    Rename the product created field to created_at

--- zerodowntime/zerodowntime/product/models.py
+++ zerodowntime/zerodowntime/product/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=250)
     description = models.JSONField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         ordering = ("pk",)
