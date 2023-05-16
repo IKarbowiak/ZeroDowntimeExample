019b679f16103040dba54ff008431035205578bc
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-16 13:34:15 +0200

    Rename the product created field to created_at

--- zerodowntime/zerodowntime/product/models.py
+++ zerodowntime/zerodowntime/product/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=255, unique=True)
     description = models.JSONField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         ordering = ("pk",)
