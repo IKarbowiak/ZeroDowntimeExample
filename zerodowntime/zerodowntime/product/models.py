db87d936993ba5265767dbe6e4328a225c878407
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-23 21:58:09 +0200

    Replace product created with created_at

--- zerodowntime/zerodowntime/product/models.py
+++ zerodowntime/zerodowntime/product/models.py
@@ -3,10 +3,8 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=255, unique=True, allow_unicode=True)
     description = models.JSONField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True, db_index=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         ordering = ("pk",)
