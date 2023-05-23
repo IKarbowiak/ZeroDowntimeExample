7bf55f745e7fadf12f165d8eb19ccccb0e212fd4
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-23 22:03:15 +0200

    Drop created from the DB

---
+++ zerodowntime/zerodowntime/product/migrations/0007_remove_created_from_db.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("product", "0006_remove_product_created_alter_product_created_at"),
+    ]
+
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL(
+                    sql="""
+                        ALTER TABLE product_product
+                        DROP COLUMN created;
+                    """,
+                )
+            ],
+        )
+    ]
