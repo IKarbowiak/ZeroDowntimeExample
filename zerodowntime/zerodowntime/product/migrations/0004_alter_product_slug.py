5d21debc1783a8afeb14dfa905df0981f46c9237
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-24 13:51:37 +0200

    Change slug into non-nullable field

---
+++ zerodowntime/zerodowntime/product/migrations/0004_alter_product_slug.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("product", "0003_populate_slug_value"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="product",
+            name="slug",
+            field=models.SlugField(max_length=255, unique=True),
+        ),
+    ]
