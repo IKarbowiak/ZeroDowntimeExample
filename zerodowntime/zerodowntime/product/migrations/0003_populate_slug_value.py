474680464d559abddc7ab259998b6cb5754111c2
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-24 13:49:01 +0200

    Copy the celery task code to the migration code in 0003_populate_slug_value

--- zerodowntime/zerodowntime/product/migrations/0003_populate_slug_value.py
+++ zerodowntime/zerodowntime/product/migrations/0003_populate_slug_value.py
@@ -1,16 +1,28 @@
-from django.apps import apps as registry
 from django.db import migrations
-from django.db.models.signals import post_migrate
+from django.db.models import F
+from django.utils.text import slugify
 
-from .tasks.v0_01 import create_slugs_for_products_task
+BATCH_SIZE = 1000
 
 
 def create_unique_slugs_for_products(apps, schema_editor):
-    def on_migrations_complete(sender=None, **kwargs):
-        create_slugs_for_products_task.delay()
+    Product = apps.get_model("product", "Product")
+    create_slugs_for_products_task(Product)
 
-    sender = registry.get_app_config("product")
-    post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
+
+def create_slugs_for_products_task(product_model):
+    # TODO: consider rewriting it to create the unique slug
+    products = product_model.objects.filter(slug__isnull=True).order_by("pk")
+    ids = products.values_list("pk", flat=True)[:BATCH_SIZE]
+    if ids:
+        product_qs = product_model.objects.filter(pk__in=ids)
+        create_slugs_for_products(product_qs)
+        create_slugs_for_products_task()
+
+
+def create_slugs_for_products(products):
+    """Create slug for Product instances."""
+    products.update(slug=slugify(F("name")))
 
 
 class Migration(migrations.Migration):
