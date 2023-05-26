33d8f6d8ed8969e77904f3b0a4c080cc0ad903ec
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-26 22:14:21 +0200

    Handle unique slugs creation

--- zerodowntime/zerodowntime/product/migrations/0003_populate_slug_value.py
+++ zerodowntime/zerodowntime/product/migrations/0003_populate_slug_value.py
@@ -1,8 +1,8 @@
 from django.db import migrations
-from django.db.models import F
 from django.utils.text import slugify
 
-BATCH_SIZE = 1000
+# Results in memory usage of ~20MB, each update takes ~ 1s
+BATCH_SIZE = 500
 
 
 def create_unique_slugs_for_products(apps, schema_editor):
@@ -11,18 +11,41 @@ def create_unique_slugs_for_products(apps, schema_editor):
 
 
 def create_slugs_for_products_task(product_model):
-    # TODO: consider rewriting it to create the unique slug
-    products = product_model.objects.filter(slug__isnull=True).order_by("pk")
-    ids = products.values_list("pk", flat=True)[:BATCH_SIZE]
-    if ids:
-        product_qs = product_model.objects.filter(pk__in=ids)
-        create_slugs_for_products(product_qs)
-        create_slugs_for_products_task()
+    products = product_model.objects.filter(slug__isnull=True).order_by("name")
+    first_char = products.first().name[0].lower()
+    products = products.filter(name__istartswith=first_char).order_by("name")
+    if products.exists():
+        slugs = list(
+            product_model.objects.filter(slug__istartswith=first_char).values_list(
+                "slug", flat=True
+            )
+        )
+        if products.count() > BATCH_SIZE:
+            ids = products.values_list("id", flat=True)[:BATCH_SIZE]
+            products = product_model.objects.filter(pk__in=ids)
+        create_slugs_for_products(product_model, products, slugs)
+        create_slugs_for_products_task(product_model)
 
 
-def create_slugs_for_products(products):
-    """Create slug for Product instances."""
-    products.update(slug=slugify(F("name")))
+def create_slugs_for_products(product_model, products, slugs):
+    """Create unique slug for Product instances."""
+    products_to_update = []
+    for product in products:
+        generate_unique_slug(product, slugs)
+        products_to_update.append(product)
+    product_model.objects.bulk_update(products_to_update, ["slug"])
+
+
+def generate_unique_slug(instance, slug_values):
+    slug = slugify(instance.name)
+    unique_slug = slug
+    extension = 1
+
+    while unique_slug in slug_values:
+        extension += 1
+        unique_slug = f"{slug}-{extension}"
+
+    return unique_slug
 
 
 class Migration(migrations.Migration):
