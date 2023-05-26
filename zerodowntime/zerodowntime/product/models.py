6411e46e605121e5ca2da9b048efd401be381b04
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-05-26 22:32:02 +0200

    Support zerodowntime for adding product slug v0.1 (#1)
    * Update readme
    
    * Add product slug field
    
    * Add migration for populating products with slug values
    
    * Handle unique slug creation
    
    * Update mutations to set the slug value during product creation

--- zerodowntime/zerodowntime/product/models.py
+++ zerodowntime/zerodowntime/product/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
     description = models.JSONField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
