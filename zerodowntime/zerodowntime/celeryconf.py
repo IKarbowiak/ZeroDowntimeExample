95cd0f4ca2e04ab2004cb7e2a0c61d09a8629d15
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-05-26 22:41:20 +0200

    Support zerodowntime for renaming created field v0.1 (#2)
    * Add product created_at field
    
    * Add migration for setting product created_at values
    
    * Set auto_now_add on product created_at
    
    * Add comments on migration and task
    
    * Add select_for_update to avoid the deadlock
    
    * Add comments to 0004_alter_product_created_at

--- zerodowntime/zerodowntime/celeryconf.py
+++ zerodowntime/zerodowntime/celeryconf.py
@@ -7,6 +7,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zerodowntime.settings")
 app = Celery("zerodowntime")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks(
+    packages=[
+        "zerodowntime.product.migrations.tasks",
+    ],
+    related_name="v0_01",
+)
 app.autodiscover_tasks()
 app.autodiscover_tasks(
     packages=[
